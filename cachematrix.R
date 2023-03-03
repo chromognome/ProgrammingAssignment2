86c718c15c6239af78f392c64bcb546c9d82bc8c
chromognome <bright.anthony@gmail.com>
2023-03-03 19:06:54 +1100

    Expand comments on functions in the list object, lexical scoping, and the assignment operator

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,17 @@
 ## This script contains R functions that are able to cache potentially time-consuming
 ## computations. If the contents of an object are not changing, it may make sense to
 ## cache the value of an operation so that when we need it again, it can be looked up
-## in the cache rather than recomputed
+## in the cache rather than recomputed.
 
-## The following function creates a special 'matrix' object that can cache its inverse
+## The following function produces a list object containing four functions:
+## 1) set()
+## 2) get()
+## 3) setInverse()
+## 4) getInverse()
+## to manipulate the values of local variables in the environment defined by
+## 'makeCacheMatrix' which remains in memory. The two set functions use the '<<-'
+## assignment operator to control the values of variables in the parent environment.
+
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -19,9 +27,12 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
-## The following function computes the inverse of the special 'matrix' returned by
-## 'makeCacheMatrix' above. If the inverse has already been calculated (and the matrix
-## has not changed), then 'cacheSolve' retrieves the inverse from the cache
+
+## The following function computes the inverse of the formal variable (a matrix)
+## defined in the 'makeCacheMatrix' object above. If the inverse has already been
+## calculated (and the matrix has not changed), then 'cacheSolve' retrieves the
+## cached value instead.
+
 
 cacheSolve <- function(y, ...) {
   j <- y$getInverse()
@@ -35,16 +46,25 @@ cacheSolve <- function(y, ...) {
   j
 }
 
-## Demonstrate use of the functions
 
-## Create special 'matrix' object
+## Demonstrate use of the functions.
+
+## Create special 'matrix' object.
 M <- makeCacheMatrix()
 
 ## Define matrix and use the functions of the special 'matrix' object. Show that this
-## object is incomplete until 'cacheSolve' is called and that the cached 'solve'
-## result is subsequently used
+## object is incomplete until 'cacheSolve' is called, and that the cached 'solve'
+## result is subsequently used.
 
 M$set(matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2))
+M$get()
+M$getInverse()
+cacheSolve(M)
+cacheSolve(M)
+
+## Change matrix and show that the inverse matrix is recalculated then cached.
+
+M$set(matrix(c(3, -2, -1, 0), nrow = 2, ncol = 2))
 M$get()
 M$getInverse()
 cacheSolve(M)
